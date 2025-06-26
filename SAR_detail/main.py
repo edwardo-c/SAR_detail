ebad05e4d1141dc9e6cadd7d75932c1f15456a0d
EdwardoCallejas <e.callejas14@yahoo.com>
2025-06-26 14:27:18 -0500

    WIP: transforming all data
    all sales data exist in single frame

--- SAR_detail/main.py
+++ SAR_detail/main.py
@@ -4,22 +4,21 @@ from scripts.SAR_pipeline import SARPipeline
 if __name__ == "__main__":
 
     sales_file_map = (
-        {"alias":"2025_all_direct", "file":SALES_PATH_2025, "sheet_name":"All Direct", "row":5},
+        {"alias":"2025_direct", "file":SALES_PATH_2025, "sheet_name":"All Direct", "row":5},
         {"alias":"2025_pos", "file":SALES_PATH_2025, "sheet_name":"POS", "row":5},
-        {"alias":"2024_all_direct", "file":SALES_PATH_2024, "sheet_name":"All Direct", "row":2},
+        {"alias":"2024_direct", "file":SALES_PATH_2024, "sheet_name":"All Direct", "row":0},
         {"alias":"2024_pos", "file":SALES_PATH_2024, "sheet_name":"POS", "row":2},
     )
 
     dimensions_file_map = (
-        {"alias": "users", "file": DIMENSIONS_PATH, "sheet_name": "users", "row":0},
-        {"alias": "pro_av", "file": DIMENSIONS_PATH, "sheet_name":"pro_av", "row": 0},
+        {"alias": "users", "file": DIMENSIONS_PATH, "sheet_name":"pro_av", "row": 0},
         {"alias": "quotas", "file": DIMENSIONS_PATH, "sheet_name":"quotas", "row": 0},
         )
 
     pipeline = SARPipeline(sales_file_map, dimensions_file_map)
     pipeline.load_data()
+    pipeline.prepare_csv()
 
-    for alias, df in pipeline.dimensions_dfs.items():
-        print(f"Info for: {alias}")
-        print(df.info())
+    print(pipeline.output_data.info())
+    print(pipeline.output_data.head())
 
--- SAR_detail/scripts/SAR_pipeline.py
+++ SAR_detail/scripts/SAR_pipeline.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
+POS_COLUMNS = [
+    "credit",
+    "reclass",
+    "distributor",
+    "customer_name",
+    "part_number",
+    "product_category",
+    "qty",
+    "amount",
+    "credit_date",
+    ]
 
 class SARPipeline():
     def __init__(self, sales_file_map: set, dimensions_file_map: set):
@@ -7,6 +18,7 @@ class SARPipeline():
         self.dimensions_file_map = dimensions_file_map
         self.sales_dfs = {}
         self.dimensions_dfs = {}
+        self.output_data = pd.DataFrame()
     
     @staticmethod
     def read_file_map(file_map):
@@ -27,9 +39,85 @@ class SARPipeline():
         self.sales_dfs = self.read_file_map(self.sales_file_map)
         self.dimensions_dfs = self.read_file_map(self.dimensions_file_map)
 
+    def _transform_pos_data():
+        ...
+    
+    def parse_alias(alias):
+        year, source = alias.split("_", 1)
+        return year, source
 
-    def transform_sales(self):
+    @staticmethod
+    def _rename_map() -> dict:
+        return {
+            "2025_direct": {
+                "Credit": "credit",
+                "Reclass" : "reclass", 
+                "Account" : "acct_num", 
+                "Customer Name": "customer_name", 
+                "Type": "pay_structure", 
+                "Inventory CD": "part_number", 
+                "Qty": "qty",  
+                "Amount": "amount", 
+                "Classification(Sales Category)": "product_category", 
+                "Invoice Date": "credit_date",
+                },
+            "2025_pos": {
+                "Credit": "credit",
+                "Reclass": "reclass",
+                "Customer": "distributor",
+                "SoldToName": "customer_name",
+                "PiiPartNumber": "part_number",
+                "PiiCategory": "product_category",
+                "ShipQuantity": "qty",
+                "ExtendedSales": "amount",
+                "PeriodDate": "credit_date",
+                },
+            "2024_direct": {
+                "2025 Credit": "credit",
+                "Customer Account Number": "acct_num",
+                "Customer Name": "customer_name",
+                "2025 SAR Rule": "pay_structure",
+                "Inventory CD": "part_number",
+                "Qty": "qty",
+                "Amount": "amount",
+                "Classification(Sales Category)": "product_category",
+                "Invoice Date": "credit_date",
+            },
+            "2024_pos": {
+                "2025 Rep": "credit",
+                "Customer": "distributor",
+                "SoldToName": "customer_name",
+                "PiiPartNumber": "part_number",
+                "PiiCategory": "product_category",
+                "ShipQuantity": "qty",
+                "ExtendedSales": "amount",
+                "PeriodDate": "credit_date"
+            },
+        }
+
+
+    @staticmethod
+    def rename_columns(df: pd.DataFrame, rename_map: dict):
         '''
+        Returns: truncated dataframe with only renamed_to columns
+        '''
+        return df.rename(columns=rename_map)[rename_map.values()]
+
+    def _process_data(self):
         
-        '''
+        rename_map = self._rename_map()
+        
+        for alias, df in self.sales_dfs.items():
+            df = self.rename_columns(df, rename_map[alias])
+            self.sales_dfs[alias] = df
+
+    def _concat_output_data(self):
+        self.output_data = pd.concat(self.sales_dfs.values(), ignore_index=True)
+
+    def prepare_csv(self):
+        self._process_data()
+        self._concat_output_data()
+        # transform direct
+        # concat
+        # add id-role-category key
         ...
